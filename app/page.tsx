e542a9871c8dde163803df945c475da8b658c86a
Nada <nadaaofficial19@gmail.com>
2023-03-01 00:28:06 +0530

    build error

--- app/page.tsx
+++ app/page.tsx
@@ -112,7 +112,10 @@ export default function Home() {
         <input
           type="checkbox"
           checked={!isJS}
-          onClick={(e) => setToJquery(!e.target.checked)}
+          onClick={(e) => {
+            const target = e.target as any;
+            setToJquery(target.checked);
+          }}
           className="peer sr-only"
         />
         <div className="peer h-6 w-11 rounded-full bg-gray-200 after:absolute after:top-[2px] after:left-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-easy-red peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-almost-easy-red dark:border-gray-600 dark:bg-gray-700 dark:peer-focus:ring-easy-red"></div>
